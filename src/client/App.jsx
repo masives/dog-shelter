8a4fe4497c4752312e3a93500e9a06acb9040a25
Jozef Piecyk <jozollo@gmail.com>
2018-11-20 21:28:39 +0100

    change app to pure component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,32 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { HashRouter, Route, Link } from 'react-router-dom';
 import AnimalsList from './components/AnimalsList/index';
 import AnimalEdit from './components/AnimalEdit';
 
-class App extends Component {
-  state = {
-    currentAnimal: '1'
-  };
-
-  render() {
-    const { currentAnimal } = this.state;
-
-    return (
-      <div>
-        <header>Guben morgen</header>
-        <HashRouter>
-          <section>
-            <div>
-              <Link to="/">Lista</Link>
-              <Link to="/animals/0">Dodaj nowego</Link>
-            </div>
-            <Route path="/" exact component={AnimalsList} />
-            <Route path="/animals/:id" exact component={AnimalEdit} />
-          </section>
-        </HashRouter>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <header>Guben morgen</header>
+    <HashRouter>
+      <section>
+        <div>
+          <Link to="/">Lista</Link>
+          <Link to="/animals/0">Dodaj nowego</Link>
+        </div>
+        <Route path="/" exact component={AnimalsList} />
+        <Route path="/animals/:id" exact component={AnimalEdit} />
+      </section>
+    </HashRouter>
+  </div>
+);
 
 export default App;
